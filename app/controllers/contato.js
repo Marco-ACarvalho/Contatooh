918a5b64f2695cdd363fa88d9c31ed5b2cb495a5
Marco-ACarvalho <marco.acarvalho76@gmail.com>
2017-05-10 18:35:59 -0300

    Version 0.05 contatooh (07-03-17)

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -18,8 +18,28 @@ var contatos = [
 
 module.exports = function() {
 	var controller = {};
+
 	controller.listaContatos = function(req, res) {
 		res.json(contatos);
 	};
+
+	controller.obtemContato = function(req, res) {
+		//console.log(req.params.id);
+
+		/* A variável filtrados sempre será um vetor, mesmo
+		se o filtro encontrar apenas um único resultado*/
+		var filtrados = contatos.filter(function(contato){
+			return contato._id == req.params.id;
+		});
+
+		if(filtrados.length > 0) { //encontrou alguém
+			//retorna o primeiro (e único) elemento do vetor
+			res.json(filtrados[0])
+		}
+		else {
+			res.status(404).send('Contato ' + req.params.id + 'não encontrado.');
+		}
+	}
+
 	return controller;
 };
