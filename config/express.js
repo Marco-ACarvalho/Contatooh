76a2751f09c97a24f7fd2fc98c71e0eebb3d4d20
Marco-ACarvalho <marco.acarvalho76@gmail.com>
2017-05-10 19:25:52 -0300

    Version 0.05 contatooh (08-03-17)

--- config/express.js
+++ config/express.js
@@ -6,7 +6,7 @@ module.exports = function() {
 	var app = express();
 	app.set('port', 3000);
 	
-	//app.use(express.static('./public'));
+	app.use(express.static('./public'));
 	
 	app.set('view engine','ejs');
 	app.set('views','./app/views');
