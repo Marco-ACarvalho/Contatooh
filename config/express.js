dc2678094fadc535c4fc4d4dff2d2a83ca3473d1
Marco-ACarvalho <marco.acarvalho76@gmail.com>
2017-05-10 15:26:16 -0300

    Segunda versão do contatooh

--- config/express.js
+++ config/express.js
@@ -3,5 +3,9 @@ var express = require('express');
 module.exports = function() {
 	var app = express();
 	app.set('port', 3000);
+	app.use(express.static('./public'));
+	app.set('view engine','ejs');
+	app.set('views','./app/views');
+	
 	return app;
 };
