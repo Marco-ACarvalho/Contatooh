c3c7d53f6540d6ddc39efd7ff73bc4c38ac182c6
Marco-ACarvalho <marco.acarvalho76@gmail.com>
2017-05-10 14:39:29 -0300

    Primeira versão do Contatooh

---
+++ config/express.js
@@ -0,0 +1,7 @@
+var express = require('express');
+
+module.exports = function() {
+	var app = express();
+	app.set('port', 3000);
+	return app;
+};
---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var app = require('./config/express')(); // Cuidado: há dois parênteses vazios aqui
+
+http
+	.createServer(app)
+	.listen(app.get('port'), function() {
+		console.log('Express escutando na porta ' + app.get('port'))
+	});
